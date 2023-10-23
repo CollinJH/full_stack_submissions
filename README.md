95a9deb480f9892eb8f25c4069b9431a42ce6183
CollinJH <cjchargreaves@gmail.com>
2023-10-22 22:00:56 -0700

    courseinfo

---
+++ README.md
@@ -0,0 +1,12 @@
+# fullstackopen submissions repo
+
+<u> repo containing my submissions for fullstackopen </u>
+
+### Material Covered in Part 0
+
+
+### Material Covered In Part 1
+
+
+
+
---
+++ part1/courseinfo/src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  const course = 'Half Stack application developement'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+
+  )
+}
